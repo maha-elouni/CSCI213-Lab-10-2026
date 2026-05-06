730c858cc298479d9a5496c68debd7ee08a89998
Maha Elouni <mahaelouni@rmc.edu>
2026-05-06 00:41:44 -0400

    initial version of CourseInfo

---
+++ CourseInfo.java
@@ -0,0 +1,17 @@
+public class CourseInfo {
+	
+	 public static void main(String[] args) {
+	        String courseName = "Object-Oriented Programming";
+	        String instructor = "Dr. Elouni";
+	        String semester = "Spring 2026";
+	        int credits = 3;
+	        String classroom = "COP 204";
+
+	        System.out.println("Course Name: " + courseName);
+	        System.out.println("Instructor: " + instructor);
+	        System.out.println("Semester: " + semester);
+	        System.out.println("Credits: " + credits);
+	        System.out.println("Classroom: " + classroom);
+	    }
+
+}
